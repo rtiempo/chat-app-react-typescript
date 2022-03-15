82dc7591a0745df04f184ed7960868a780976135
Robert Tiempo <jroberttiempo58@gmail.com>
2022-03-15 23:26:20 +0800

    Remove react-firebaseui dependency

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -25,34 +25,6 @@ const Login = () => {
     console.log(data);
     setUser(await signInWithEmailAndPassword(auth, email, password));
   }
-  // const ui = new firebaseui.auth.AuthUI(auth());
-  // ui.start('#firebaseui-auth-container', {
-  //   signInOptions: [
-  //     auth.EmailAuthProvider.PROVIDER_ID
-  //   ],
-  // });
-  // const config = {
-  //   apiKey: 'AIzaSyANOcjT3J-1d_XCJU92lolDXHwCOcwHMUc',
-  //   authDomain: 'chat-app-react-firebase-317dc.firebaseapp.com',
-  //   projectId: 'chat-app-react-firebase-317dc',
-  //   storageBucket: 'chat-app-react-firebase-317dc.appspot.com',
-  //   messagingSenderId: '746948682150',
-  //   appId: '1:746948682150:web:ea71ff33f6690aa9fcef67',
-  // }
-  // firebase.initializeApp(config);
-  
-  // const uiConfig = {
-  //   signInFlow: 'popup',
-  //   signInSuccessUrl: '/signedIn',
-  //   signInOptions: [
-  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  //   ],   
-  // }
-
-  // const handleLogin = () => {
-  //   const provider = new firebase.auth.GoogleAuthProvider()
-  //   auth.signInWithPopup(provider);
-  // }
 
   return (
     <LoginContainer>
